8a73c26ce306a60243e2ab1553519b3b3238747d
matsushinDB11 <matsushin35@gmail.com>
2021-05-12 17:04:33 +0900

    add .gitignore to .vscode/setting.json

--- abc087b/abc087b.cpp
+++ abc087b/abc087b.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 int main() {
   int a, b, c, x;
-  cin >> a;
-  cin >> b;
-  cin >> c;
-  cin >> x;
-  cout << a << b << c << x;
+  cin >> a >> b >> c >> x;
+  // cout << a << b << c << x;
+
+
   return 0;
 }
