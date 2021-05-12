8ca3a790d881e13e5065bf4952ae6e4a6c74c2d5
matsushinDB11 <matsushin35@gmail.com>
2021-05-12 18:49:04 +0900

    done abc087b

--- abc087b/abc087b.cpp
+++ abc087b/abc087b.cpp
@@ -4,8 +4,26 @@ using namespace std;
 int main() {
   int a, b, c, x;
   cin >> a >> b >> c >> x;
+  int counter = 0;
   // cout << a << b << c << x;
-
+  for (int i = 0; i <= a; i++)
+  {
+    for (int j = 0; j <= b; j++)
+    {
+      for (int k = 0; k <= c; k++)
+      {
+        if (i*500 + j*100 + k*50 == x)
+        {
+          counter++;
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+cout << counter << endl;
 
   return 0;
 }
