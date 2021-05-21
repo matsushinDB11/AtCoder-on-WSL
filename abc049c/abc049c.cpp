400bc88190934eb8f896f11c8b8afa53b65edc28
matsushinDB11 <matsushin35@gmail.com>
2021-05-21 12:29:59 +0900

    add abc049c

---
+++ abc049c/abc049c.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    // cout << s << endl;
+    string devider[4] = {"dream", "dreamer", "erase", "eraser"};
+    queue<int> bfs;
+    bfs.push(0);
+    while (!bfs.empty())
+    {
+        /* code */
+    }
+    
+    return 0;
+}
